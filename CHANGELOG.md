d58c6b8bd759f69bf866d03f752997dc5444aa34
Ashur Cabrera <hello@cabreramade.co>
2016-12-29 14:11:39 -0700

    added upgrade command (fixes #2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,4 @@ All notable changes to Jelly will be documented in this file.
 ## [Unreleased]
 ### Added
 - CHANGELOG support to the 'init' command
+- `upgrade` command
--- lib/helpers/application.php
+++ lib/helpers/application.php
@@ -3,6 +3,11 @@
 use Cranberry\Core\File;
 
 /*
+ * Set config
+ */
+$app->registerCommandObject( 'repoURL', $config['url'] );
+
+/*
  * Set data directory
  */
 // $dataDirectory = new File\Directory( '...' );
